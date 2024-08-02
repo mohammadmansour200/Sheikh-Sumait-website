e4983c0c96c9947e8b6f3778f3fbe6b6e2f846fe
Mohammad Mansour <abdultawab940@gmail.com>
2024-08-02 15:34:30 +0300

    made a lot of things :)

--- script.js
+++ script.js
@@ -2,35 +2,39 @@ const themeToggle = document.querySelector("#theme-toggle");
 const backtotop = document.querySelector("#backtotop");
 
 function enableDarkMode() {
-  document.body.classList.remove("light-theme");
-  document.body.classList.add("dark-theme");
-  themeToggle.setAttribute("aria-label", "التحويل للوضع الفاتح");
+	document.body.classList.remove("light-theme");
+	document.body.classList.add("dark-theme");
+	themeToggle.setAttribute("aria-label", "التحويل للوضع الفاتح");
 }
 
 function enableLightMode() {
-  document.body.classList.add("light-theme");
-  document.body.classList.remove("dark-theme");
-  themeToggle.setAttribute("aria-label", "التحويل للوضع الداكن");
+	document.body.classList.add("light-theme");
+	document.body.classList.remove("dark-theme");
+	themeToggle.setAttribute("aria-label", "التحويل للوضع الداكن");
 }
 
 document.body.classList.contains("light-theme")
-  ? enableLightMode()
-  : enableDarkMode();
+	? enableLightMode()
+	: enableDarkMode();
 
 themeToggle.addEventListener("click", () => {
-  document.body.classList.contains("light-theme")
-    ? enableDarkMode()
-    : enableLightMode();
+	document.body.classList.contains("light-theme")
+		? enableDarkMode()
+		: enableLightMode();
 });
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 600) {
-    backtotop.style.visibility = "visible";
-    backtotop.style.transform = "translateY(1%)";
-    backtotop.style.transition =
-      "transform 500ms cubic-bezier(0.11, 0.14, 0.29, 1.32)";
-  } else {
-    backtotop.style.visibility = "hidden";
-    backtotop.style.transform = "translateY(8%)";
-  }
+	if (window.scrollY > 600) {
+		backtotop.style.opacity = "100";
+		backtotop.style.visibility = "visible";
+		backtotop.style.transform = "translateY(1%)";
+		backtotop.style.transition =
+			"transform 500ms cubic-bezier(0.11, 0.14, 0.29, 1.32)";
+	} else {
+		backtotop.style.opacity = "0";
+		backtotop.style.transform = "translateY(8%)";
+		backtotop.style.visibility = "hidden";
+		backtotop.style.transition =
+			"all 500ms cubic-bezier(0.11, 0.14, 0.29, 1.32)";
+	}
 });
